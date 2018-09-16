cfbe179b4e24d5d39da83ceec8d9747531874bfe
Pavel Balusek <mr.p.balusek@gmail.com>
2018-09-16 19:50:31 +0200

    Project configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import org.opencv.core.Core;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     static{
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        nu.pattern.OpenCV.loadShared();
     }
-
+    public static void main(String[] args) {
+        File pictures= new File("./pictures/");
+        pictures.mkdirs();
+        System.out.println(pictures.getPath());
+        try {
+            FileSaver.createFile(pictures.getPath()+"/kokot.txt", "kokot");
+        } catch (IOException ignore) {
+        }
+    }
 }
